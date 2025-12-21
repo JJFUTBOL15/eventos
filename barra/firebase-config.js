2918b87d685f000997e134fbc4ef3a5479c874bc
Silva <salinasmarinjosejavier20@gmail.com>
2025-12-21 13:55:55 -0500

    Create firebase-config.js

---
+++ barra/firebase-config.js
@@ -0,0 +1,15 @@
+// barra/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBceYCUr-PyJl0G1E4wS2dGwBidz_Rn-cc",
+  authDomain: "jjfutbol-b07c0.firebaseapp.com",
+  databaseURL: "https://jjfutbol-b07c0-default-rtdb.firebaseio.com",
+  projectId: "jjfutbol-b07c0",
+  storageBucket: "jjfutbol-b07c0.firebasestorage.app",
+  messagingSenderId: "99569181408",
+  appId: "1:99569181408:web:807cd92e9cea82c19a2685"
+};
+
+// Evitar inicialización múltiple
+if (typeof firebase !== 'undefined' && !firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
